d018be1d5ab7f06c6df45ba7d246d6fc0e2351df
Alexandre Di Guida <Alex@Alexandres-MacBook-Pro.local>
2016-12-17 09:42:37 +0000

    Firebase notif

--- app/src/test/java/com/android/alex/firebasenotification/GetTimeMilliSecTest.java
+++ app/src/test/java/com/android/alex/firebasenotification/GetTimeMilliSecTest.java
@@ -1,8 +1,43 @@
 package com.android.alex.firebasenotification;
 
+import com.android.alex.firebasenotification.util.NotificationUtils;
+
+import org.joda.time.DateTime;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Alex on 15/11/2016.
  */
 
 public class GetTimeMilliSecTest {
+
+    String correctTimestamp;
+    String wrongTimestamp;
+
+    @Before
+    public void setup() throws ParseException {
+        correctTimestamp = "2016-11-15 13:25:00";
+        wrongTimestamp = "13:25:00 2016-11-15";
+    }
+
+    @Test
+    public void getTimeMilliSec_correct() throws Exception {
+        long result = NotificationUtils.getTimeMilliSec(correctTimestamp);
+        assertTrue("Error!", result > 0);
+    }
+
+    @Test
+    public void getTimeMilliSec_error() {
+        long result = NotificationUtils.getTimeMilliSec(wrongTimestamp);
+        assertTrue("Error!", result == 0);
+    }
 }
